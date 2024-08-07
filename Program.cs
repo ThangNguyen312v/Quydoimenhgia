1cf342c33f62133cfcaafffd72aaa185cd199c38
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-07 14:51:21 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace quydoitiente{
+    class Program{
+        static void Main(string[] args){
+            float USD;
+            float VND;
+            float rate = 25150;
+            
+            Console.WriteLine("nhap so tien muon quy doi: ");
+            USD = Int16.Parse(Console.ReadLine());
+            VND = USD * rate;
+            Console.WriteLine("So tien quy doi la: " + VND);
+            
+
+
+        }
+    }
+}
